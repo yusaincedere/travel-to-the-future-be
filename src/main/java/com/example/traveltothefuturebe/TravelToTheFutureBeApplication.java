464f06e44084e8a050fe4783985a3181e324857d
yusaincedere <yusaincedere@gmail.com>
2023-08-16 23:52:30 +0300

    project created

---
+++ src/main/java/com/example/traveltothefuturebe/TravelToTheFutureBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.traveltothefuturebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelToTheFutureBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelToTheFutureBeApplication.class, args);
+    }
+
+}

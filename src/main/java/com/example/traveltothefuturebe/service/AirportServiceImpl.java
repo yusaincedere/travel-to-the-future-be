8a7ce08850d38be9a5bb7fa64e4048351e7f0c2b
yusaincedere <yusaincedere@gmail.com>
2023-08-17 23:00:17 +0300

    Service Classes created for Airport and Flight

---
+++ src/main/java/com/example/traveltothefuturebe/service/AirportServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.traveltothefuturebe.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AirportServiceImpl implements AirportService{
+}
---
+++ src/main/java/com/example/traveltothefuturebe/service/FlightService.java
@@ -0,0 +1,4 @@
+package com.example.traveltothefuturebe.service;
+
+public interface FlightService{
+}

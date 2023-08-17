cf51ab2b0b2a214253325ac7be413a491e798f6d
yusaincedere <yusaincedere@gmail.com>
2023-08-17 23:32:38 +0300

    Added CustomNotFoundException

---
+++ src/main/java/com/example/traveltothefuturebe/common/ExceptionConstants.java
@@ -0,0 +1,6 @@
+package com.example.traveltothefuturebe.common;
+
+public class ExceptionConstants {
+    public static final String FLIGHT_NOT_FOUND_MESSAGE = "Flight not found";
+    public static final String AIRPORT_NOT_FOUND_MESSAGE = "Airport not found";
+}
